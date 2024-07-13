cff34536965f4faa54c50f6cea056e56bcb8267b
SultanTipa1 <josephngumbaugush@gmail.com>
2024-07-13 22:07:05 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let pi = 3.14159;
+//=> undefined
+
+let pi = "the ratio between a circle's circumference and diameter";
+//=> "the ratio between a circle's circumference and diameter"
+
+const pi;
+//=> Uncaught SyntaxError: Missing initializer in const declaration
+
+const pi = 3.14159;
+//=> undefined
+
